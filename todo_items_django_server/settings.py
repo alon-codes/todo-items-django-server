9dbdb6425a7adb435f4404a045e2c6b11c2d7cd4
Alon Dubov <alon@dubov.tech>
2023-08-28 18:31:24 +0300

    Updating db name

--- todo_items_django_server/settings.py
+++ todo_items_django_server/settings.py
@@ -9,13 +9,13 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
-
 import os
 from pathlib import Path
+from dotenv import load_dotenv
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
@@ -84,10 +84,12 @@ WSGI_APPLICATION = "todo_items_django_server.wsgi.application"
 
 DATABASES = {
   'default': {
-    'ENGINE': 'django.db.backends.sqlite',
-    'NAME': BASE_DIR / "db.sqlite",
+    'ENGINE': 'django.db.backends.sqlite3',
+    'NAME': BASE_DIR / "db.sqlite3",
   }
 }
+
+print(DATABASES)
 
 
 # Password validation
