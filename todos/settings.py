4d72106ca9a3edb5449a0b5c0784fafd255f63ce
Alon Dubov <alon@dubov.tech>
2023-09-25 19:07:20 +0300

    Removing db creation

--- todos/settings.py
+++ todos/settings.py
@@ -80,18 +80,12 @@ WSGI_APPLICATION = "todos.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-f = open(BASE_DIR / "db.sqlite3", "w")
-f.close()
-
 DATABASES = {
   'default': {
     'ENGINE': 'django.db.backends.sqlite3',
     'NAME': BASE_DIR / "db.sqlite3",
   }
 }
-
-print(DATABASES)
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
--- todos/views.py
+++ todos/views.py
@@ -1,8 +1,6 @@
-from django.shortcuts import render
 from rest_framework import viewsets
-
-from todos.models import Todo
-from todos.serializers import TodoSerializer
+from .models import Todo
+from .serializers import TodoSerializer
 
 # Create your views here.
 class TodosViewSet(viewsets.ModelViewSet):
